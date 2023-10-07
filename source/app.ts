0a3c32927a8d6dbce4305a638cffcb3a4cb8603a
Robson Arita <robsonkeniti@gmail.com>
2023-10-07 20:13:06 -0300

    moved server to source rep

--- source/app.ts
+++ source/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import http from 'http'
 import { initializeDatabase } from './database'
-import { initializeServer } from '../server'
+import { initializeServer } from './server'
 
 dotenv.config()
 
