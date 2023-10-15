b3ab5ae933150a8d7e256828601bec6de07606dd
Robson Arita <robsonkeniti@gmail.com>
2023-10-15 15:06:09 -0300

    add cors to specific domain

--- source/middleware.ts
+++ source/middleware.ts
@@ -1,10 +1,20 @@
 import { Router, json, urlencoded } from 'express'
 import terminalConsole from './middlewares/TerminalConsole'
 import CustomResponse from './customResponse/CustomResponse'
+import cors from 'cors'
 
 let customResponse: CustomResponse 
 
 export const initializeMiddlewares = (app: Router) => {
+  const ip = process.env.IP
+  const port = process.env.PORT
+  app.use(cors({
+    origin: `http://25.18.66.27:5000`,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  }))
+
   app.use(json())
   app.use(urlencoded({ extended: false }))
   app.use(terminalConsole)
